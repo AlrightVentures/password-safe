44ea432115268af733769c25b54b9723b778228f
Zatarra <58144826+AlrightVentures@users.noreply.github.com>
2023-01-23 23:22:54 +0100

    corrected comments

--- passwordSafe.py
+++ passwordSafe.py
@@ -9,7 +9,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 # Define functions 
-# Function creates a key - run it once to create key.key file
+# Function creates a key - run it once to create key.key file that will store your key
 """def createKey():
     key = Fernet.generate_key()
 
@@ -22,29 +22,39 @@ createKey()
 # Function retrieves key data from a file
 def openKey():
 
+    # Open the key,key file in the read bytes mode
     file = open("key.key", "rb")
+    # Read data from the file
     key = file.read()
+    # Close the file to avoid problems
     file.close()
+    # Return key to be used 
     return key
 
 
 # Add function allows user to add new account and password to their safe
 def add():
     
+    # User to add name of the account and password they want to store in the safe
     name = input("Name of your new account: ")
     password = input("Password: ")
 
+    # Add new info to the end of the file storing passwords
     with open("passwords.txt", "a") as file: 
         file.write(name + "|" + fer.encrypt(password.encode()).decode() + "\n")
 
 # View function allows users to add new account name and password to their password safe
 def view():
 
+    # Open the file storing passwords
     with open("passwords.txt", "r") as file: 
+        # Read the file line by line 
         for line in file.readlines():
+            # Strip the \n character from the line
             info = line.rstrip()
-            user, passw = info.split("|")
-            print(f"User:  {user}\nPassword: {fer.decrypt(passw.encode()).decode()}")
+            # Store account, password info in separate variables
+            account, passw = info.split("|")
+            print(f"Account:  {account}\nPassword: {fer.decrypt(passw.encode()).decode()}")
 
 
 # TODO: Ask user for their main password to unlock the account 
@@ -64,13 +74,18 @@ while True:
     # Exit the software 
     if userChoice == "q": 
         break
-
+    # Run add function
     elif userChoice == "add":
         add()
-
+    # Run view function
     elif userChoice == "view":
         view()
-
+    # User submitted invalid option. Ask user to provide a valid option. Goes back to the start of loop.
     else:
         print("Please pick a valid option.\n")
         continue
+
+
+# Made with love.
+# Check out Harry Mack on YouTube. 
+# Have a great day.
